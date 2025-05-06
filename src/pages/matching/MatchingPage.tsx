0ba3e302eb57e37c7c54d9a14123382143b91815
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-05-06 21:28:55 +0900

    fix: 시간초 원복

--- src/pages/matching/MatchingPage.tsx
+++ src/pages/matching/MatchingPage.tsx
@@ -31,7 +31,7 @@ const MatchingPage: React.FC = () => {
           } else {
             navigate("/main", { replace: true }); // fallback
           }
-        }, 2750);
+        }, 3000);
       }
     }, 3000);
 
