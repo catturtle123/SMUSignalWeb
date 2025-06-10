d5312ac5c1f6b325b1a2a1512c1cd9e33766f00e
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-06-10 15:31:38 +0900

    a

--- src/pages/main/Mainpage.tsx
+++ src/pages/main/Mainpage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, FormEvent } from "react";
+import { useState, FormEvent } from "react";
 import axios, { AxiosError } from "axios";
 import { useNavigate } from "react-router-dom";
 import ghost from "../../assets/ghost.svg";
